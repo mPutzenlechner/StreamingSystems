13b9e84f7fbe2603ba6810d37c49ab732f9f2139
mpr <moputzenlechner@pm.me>
2024-10-11 10:43:06 +0200

    Kafka example works

--- GettingStarted/src/main/java/org/example/KafkaConnector.java
+++ GettingStarted/src/main/java/org/example/KafkaConnector.java
@@ -1,11 +1,35 @@
 package org.example;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.*;
 
+import java.util.Properties;
+
+// A simple connector demonstrating functionality of the kafka server
 public class KafkaConnector {
 
-    KafkaConnector (
+    private final Producer<String, String> producer;
 
-    ) {
+    KafkaConnector () {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "172.17.0.3:9092");
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        this.producer = new KafkaProducer<String, String>(properties);
+    }
 
+    public void sendSomeData() {
+        String topicName = "testTopic";
+        for (int i = 0; i < 10; i++) {
+            this.producer.send(
+                    new ProducerRecord<String, String>(topicName, "test", Integer.toString(i))
+            );
+        }
+        this.producer.close();
     }
 }
--- GettingStarted/src/main/java/org/example/Main.java
+++ GettingStarted/src/main/java/org/example/Main.java
@@ -4,10 +4,11 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
 
+        System.out.println("Send some Kafka data");
+        KafkaConnector kafkaConnector = new KafkaConnector();
+        kafkaConnector.sendSomeData();
 
     }
 }
