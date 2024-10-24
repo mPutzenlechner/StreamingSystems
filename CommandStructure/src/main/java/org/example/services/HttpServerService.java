0e71d061554b32e802277999d4afcc9db55da393
mpr <moputzenlechner@pm.me>
2024-10-24 19:31:25 +0200

    query side now has an http interface as well

--- CommandStructure/src/main/java/org/example/services/HttpServerService.java
+++ CommandStructure/src/main/java/org/example/services/HttpServerService.java
@@ -1,17 +1,21 @@
 package org.example.services;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.Message;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.example.commandhandler.VehicleCommandHandler;
 import org.example.commands.*;
 import org.example.domainmodel.Position;
+import org.example.querymodel.QueryHandler;
+import org.example.querymodel.VehicleDTO;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import java.net.URI;
+import java.util.Collection;
 
 
 @Path("commands")
@@ -71,5 +75,49 @@ public class HttpServerService {
         }
         this.logger.debug("Command created for HttpRequest \"remove vehicle\".");
         return Response.ok().build();
+    }
+
+    @GET
+    @Path("/byname/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getVehicle(@PathParam("name") String name) {
+        try {
+            VehicleDTO vehicleDTO = QueryHandler.getInstance().getVehicleByName(name);
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(vehicleDTO);
+        } catch (Exception e) {
+            this.logger.error("HttpRequest \"get vehicle\" failed with error: {}", e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllVehicles() {
+        try {
+            Collection<VehicleDTO> vehicleDTOCollection = QueryHandler.getInstance().getVehicles();
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(vehicleDTOCollection);
+        } catch (Exception e) {
+            this.logger.error("HttpRequest \"get vehicles\" failed with error: {}", e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    @GET
+    @Path("/at/{x}/{y}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getVehiclesAtPosition(@PathParam("x") int x, @PathParam("y") int y) {
+        try {
+            Collection<VehicleDTO> vehicleDTOCollection = QueryHandler.getInstance().getVehiclesAtPosition(
+                    new Position(x, y)
+            );
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(vehicleDTOCollection);
+        } catch (Exception e) {
+            this.logger.error("HttpRequest \"get vehicles at position\" failed with error: {}", e.getMessage());
+            return e.getMessage();
+        }
     }
 }
