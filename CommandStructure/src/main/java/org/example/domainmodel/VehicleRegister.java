2c30f6ab4cec3b90250dcea0713982ef653c2714
mpr <moputzenlechner@pm.me>
2024-11-04 09:14:50 +0100

    removed unnecessary dependencies

--- CommandStructure/src/main/java/org/example/domainmodel/VehicleRegister.java
+++ CommandStructure/src/main/java/org/example/domainmodel/VehicleRegister.java
@@ -1,9 +1,5 @@
 package org.example.domainmodel;
-import org.example.querymodel.VehicleDTO;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
