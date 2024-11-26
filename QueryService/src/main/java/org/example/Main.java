4054bb4926b135ada04a443090c31de5f5836e06
mpr <moputzenlechner@pm.me>
2024-11-26 17:03:30 +0100

    ip changes

--- QueryService/src/main/java/org/example/Main.java
+++ QueryService/src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Projector projector = Projector.getInstance();  // Start projector
         HttpServerService httpServerService = new HttpServerService();
-        httpServerService.startHttpServer("http://localhost:8080/api/");
+        httpServerService.startHttpServer("http://0.0.0.0:8081/api/");
         // System.out.println("Hit enter to stop the server...");
         // System.in.read();
         // httpServerService.stopHttpServer();
--- WriteService/src/main/java/org/example/Main.java
+++ WriteService/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         HttpServerService httpServerService = new HttpServerService();
-        httpServerService.startHttpServer("http://localhost:8080/api/");
+        httpServerService.startHttpServer("http://0.0.0.0:8080/api/");
         // System.out.println("Hit enter to stop the server...");
         // System.in.read();
         // httpServerService.stopHttpServer();
